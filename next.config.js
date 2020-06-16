fe3005ab7f882787ed2fce6f15bf91578caba08c
Eric Bichan <eric.bichan@gmail.com>
2020-06-16 15:52:18 -0400

    added css modules, post css rules, removed tailwind

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 // next.config.js
 const withFonts = require("next-fonts");
-module.exports = withFonts();
+const withCSS = require("@zeit/next-css");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+module.exports = withFonts(
+  withCSS({
+    cssModules: true,
+    cssLoaderOptions: {
+      importLoaders: 1,
+      localIdentName: isProduction
+        ? "[hash:base64:5]"
+        : "[name]__[local]___[hash:base64:5]",
+    },
+  })
+);
