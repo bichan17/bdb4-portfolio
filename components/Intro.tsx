4828cb151db5e59cce6bf52fb9638a330f2ae74f
Eric Bichan <eric.bichan@gmail.com>
2020-06-16 14:48:01 -0400

    added font files, css vars files

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,10 +1,11 @@
 import { CMS_NAME } from "../lib/constants";
+import styles from "./Intro.module.css";
 
 const Intro = () => {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <section className={styles.root}>
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Intro.
+        Eric Bichan
       </h1>
     </section>
   );
