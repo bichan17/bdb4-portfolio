091bd3056d9f79a88a0506c055956ffa5069cb44
Eric Bichan <eric.bichan@gmail.com>
2020-06-17 14:22:01 -0400

    added base styles, intro content

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -4,7 +4,19 @@ import styles from "./Intro.module.css";
 const Intro = () => {
   return (
     <section className={styles.root}>
-      <h1>Eric Bichan</h1>
+      <h1>Hi, how are you?</h1>
+      <p>
+        I’m Eric Bichan, a web developer based in Brooklyn, NY, specializing in
+        projects in the arts and media space. Here is my <a href="#">resume</a>.
+      </p>
+
+      <p>
+        I am currently freelance, previously with <a href="#">BBH NY</a>.
+      </p>
+
+      <p>
+        Want to make something cool? Feel free to <a href="#">reach out</a>.
+      </p>
     </section>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import "../styles/layout.css";
+import "../styles/global.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
