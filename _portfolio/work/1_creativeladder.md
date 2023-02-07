1fd1c208b2bea917848691711e662942f87ba66a
Eric Bichan <eric.bichan@gmail.com>
2023-02-07 11:28:40 -0500

    feat: update site content

---
+++ _portfolio/work/1_creativeladder.md
@@ -0,0 +1,13 @@
+---
+project_link: https://theguide.art/
+title: "Creative Ladder Career Finder"
+date: "January 2023"
+tags:
+  - React
+  - Next.js
+  - Sanity
+  - Framer Motion
+  - Storybook
+---
+
+A simple online quiz that takes just minutes to complete, testing the personality of respondents and matching them with creative careers that suit them. It also serves as a resource for learning about the jobs, skills, and tools needed across creative industries.
--- next.config.js
+++ next.config.js
@@ -1,22 +1,3 @@
-// next.config.js
-// const withFonts = require("next-fonts");
-// const withCSS = require("@zeit/next-css");
-
-// const isProduction = process.env.NODE_ENV === "production";
-
-// module.exports = withFonts(
-//   withCSS({
-//     cssModules: true,
-//     cssLoaderOptions: {
-//       importLoaders: 1,
-//       localIdentName: isProduction
-//         ? "[hash:base64:5]"
-//         : "[name]__[local]___[hash:base64:5]",
-//     },
-//   })
-// );
-
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
