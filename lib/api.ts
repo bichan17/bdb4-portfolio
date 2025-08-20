fb709fa1dd9d96f4bdd12b05c4f878e9b31662bd
Eric Bichan <eric.bichan@gmail.com>
2025-08-20 16:10:14 -0400

    add ts-ignore

--- lib/api.ts
+++ lib/api.ts
@@ -11,6 +11,7 @@ export function getPostSlugsByType(type: string) {
 function isNumeric(str: string) {
   if (typeof str != "string") return false; // we only process strings!
   return (
+    // @ts-ignore
     !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
     !isNaN(parseFloat(str))
   ); // ...and ensure strings of whitespace fail
