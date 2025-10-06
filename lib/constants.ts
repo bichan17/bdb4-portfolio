16e85519f61462eba2aa0869352e56c898ba6f30
Eric Bichan <eric.bichan@gmail.com>
2025-10-06 17:23:37 -0400

    update resume

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const HOME_OG_IMAGE_URL = "/assets/images/OG2.png";
 export const EMAIL_ADDRESS = "eric.bichan@gmail.com";
 export const GITHUB_URL = "https://github.com/bichan17/";
-export const RESUME = "/assets/files/Eric_Bichan_Resume_2025.pdf";
+export const RESUME = "/assets/files/Eric_Bichan_Resume_2025_ATS.pdf";
